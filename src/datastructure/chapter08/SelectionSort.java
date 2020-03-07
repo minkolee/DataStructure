b265aefca778f8bcb316263644bbe603a6b83131
minkolee <minkolee@gmail.com>
2020-03-07 19:05:26 +0800

    选择排序重构完成

--- src/datastructure/chapter08/SelectionSort.java
+++ src/datastructure/chapter08/SelectionSort.java
@@ -1,29 +1,221 @@
 package datastructure.chapter08;
 
+/**
+ * 选择排序类
+ */
 public class SelectionSort {
 
-    public static <T extends Comparable<? super T>> void sort(T[] array, int n) {
-        if (n > array.length) {
-            throw new RuntimeException("数组的项比参数n要少!");
-        }
-        if (!(n == 0 || n == 1)) {
-            //对数组中每一个元素, 先找到这个元素到结尾的最小值, 然后和自己替换
-            for (int i = 0; i < n; i++) {
-                int minindex = findMin(array, i, n - 1);
+    /**
+     * 给数组开头的count个元素进行升序排列
+     *
+     * @param array 数组
+     * @param count 从开始要排序的元素的数量
+     * @param <T>   泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortFromStart(T[] array, int count) {
 
-                //交换两个元素的位置
-                T temp = array[i];
-                array[i] = array[minindex];
-                array[minindex] = temp;
-            }
+        //判断参数是否合理, 由于核心函数会判断参数是否有问题, 所以这里只需要判断count即可, 等于0或者1直接返回
+        //如果count等于其他值, 会由核心函数进行判断
+        if (count == 0 || count == 1) {
+            return;
         }
+
+        //调用核心函数, 开始索引为0, 结束索引为count-1即可
+        sort(array, 0, count - 1, false);
     }
 
+    /**
+     * 给数组开头的count个元素进行降序排列
+     *
+     * @param array 数组
+     * @param count 从开始要排序的元素的数量
+     * @param <T>   泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortFromStartDesc(T[] array, int count) {
+
+        //判断参数是否合理, 由于核心函数会判断参数是否有问题, 所以这里只需要判断count即可, 等于0或者1直接返回
+        //如果count等于其他值, 会由核心函数进行判断
+        if (count == 0 || count == 1) {
+            return;
+        }
+
+        //调用核心函数, 开始索引为0, 结束索引为count-1即可
+        sort(array, 0, count - 1, true);
+    }
+
+
+    /**
+     * 给从数组末尾向前数起的count个元素进行升序排序
+     *
+     * @param array 要排序的数组
+     * @param count 末尾的count个元素
+     * @param <T>   泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortFromEnd(T[] array, int count) {
+
+        //判断参数是否合理, 由于核心函数会判断参数是否有问题, 所以这里只需要判断count即可, 等于0或者1直接返回
+        //如果count等于其他值, 会由核心函数进行判断
+        if (count == 0 || count == 1) {
+            return;
+        }
+
+        //调用核心函数, 开始索引为array.length-1-count, 结束索引为 array.length-1
+        recursionSort(array, array.length - count, array.length - 1, false);
+    }
+
+    /**
+     * 给从数组末尾向前数起的count个元素进行降序排序
+     *
+     * @param array 要排序的数组
+     * @param count 末尾的count个元素
+     * @param <T>   泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortFromEndDesc(T[] array, int count) {
+
+        //判断参数是否合理, 由于核心函数会判断参数是否有问题, 所以这里只需要判断count即可, 等于0或者1直接返回
+        //如果count等于其他值, 会由核心函数进行判断
+        if (count == 0 || count == 1) {
+            return;
+        }
+
+        //调用核心函数, 开始索引为array.length-1-count, 结束索引为 array.length-1
+        recursionSort(array, array.length - count, array.length - 1, true);
+    }
+
+    /**
+     * 对数组全部元素进行升序排列, 内部直接调用核心私有方法或者另外一个排序前N个的都可以
+     *
+     * @param array 要排序的数组
+     * @param <T>   泛型参数,必须实现Comparable接口
+     */
     public static <T extends Comparable<? super T>> void sort(T[] array) {
-        sort(array, array.length);
+        sort(array, 0, array.length - 1, false);
     }
 
-    private static <T extends Comparable<? super T>> int findMin(T[] array, int startIndex, int lastIndex) {
+    /**
+     * 对数组全部元素进行降序排列, 内部直接调用核心私有方法或者另外一个排序前N个的都可以
+     *
+     * @param array 要排序的数组
+     * @param <T>   泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortDesc(T[] array) {
+        sort(array, 0, array.length - 1, true);
+    }
+
+
+    /**
+     * 对指定索引范围内的数组元素进行升序排序, 包含起始索引与结束索引
+     *
+     * @param array      要排序的数组
+     * @param startIndex 要排序的部分的起始索引
+     * @param endIndex   要排序的部分的结束索引
+     * @param <T>        泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortBetweenIndex(T[] array, int startIndex, int endIndex) {
+        //内部直接调用私有核心方法即可
+        sort(array, startIndex, endIndex, false);
+    }
+
+    /**
+     * 对指定索引范围内的数组元素进行降序排序, 包含起始索引与结束索引
+     *
+     * @param array      要排序的数组
+     * @param startIndex 要排序的部分的起始索引
+     * @param endIndex   要排序的部分的结束索引
+     * @param <T>        泛型参数,必须实现Comparable接口
+     */
+    public static <T extends Comparable<? super T>> void sortBetweenIndexDesc(T[] array, int startIndex, int endIndex) {
+        //内部直接调用私有核心方法即可
+        sort(array, startIndex, endIndex, true);
+    }
+
+
+    /**
+     * 对起始索引索引和结束索引之间(包含起始索引和结束索引)的部分进行排序, 就地排序, 不返回新数组.
+     * 本方法是数组排序的核心私有方法, 对外暴露的其他方法内部调用该方法.
+     * 数组元素需要实现Comparable接口
+     *
+     * @param array      要排序的数组,
+     * @param startIndex 要进行排序部分的起始索引
+     * @param endIndex   要进行排序部分的结束索引
+     * @param reverse    布尔类型, 是否降序排序, 为true表示降序排序, 为false表示升序排序
+     * @param <T>        泛型参数,必须实现Comparable接口
+     */
+    private static <T extends Comparable<? super T>> void sort(T[] array, int startIndex, int endIndex, boolean reverse) {
+        //判断数组长度, 如果为0或者1, 直接返回
+        if (array.length == 0 || array.length == 1) {
+            return;
+        }
+
+        //判断两个索引参数是否符合要求
+        if (startIndex < 0 || startIndex > array.length - 1 || endIndex < 0 || endIndex > array.length - 1 || startIndex > endIndex) {
+            throw new IllegalArgumentException("索引超出范围");
+        }
+
+        //主排序算法, 对于数组中每一个元素, 获取其到endIndex之间的最小值, 然后交换二者的位置, 注意, 其实只需要遍历到倒数第二个元素即可, 因为不管交换不交换, 结果都正确.
+        for (int i = startIndex; i < endIndex; i++) {
+            //根据是否升序或者降序, 查找当前索引到结束索引之间的最小/大值对应的索引
+            int swapIndex;
+            if (reverse) {
+                swapIndex = findMaxIndex(array, i, endIndex);
+            } else {
+                swapIndex = findMinIndex(array, i, endIndex);
+            }
+
+            //交换当前索引和最大/最小值索引的内容
+            swap(array, i, swapIndex);
+        }
+    }
+
+    /**
+     * 递归版本的核心排序私有方法, 方法原理是, 寻找当前数组的最小/大值, 与第一个进行交换. 然后对数组的第二个元素开始到结束的部分进行同样的空操作, 直到剩余部分长度为1结束.
+     * 这个递归方法的名称应该叫交换第一个元素与最大/最小值更加合适
+     *
+     * @param array      要排序的数组
+     * @param startIndex 开始的索引
+     * @param endIndex   结束的索引
+     * @param reverse    是否反向(降序)排列, true为降序, false为升序
+     * @param <T>        泛型参数,必须实现Comparable接口
+     */
+    private static <T extends Comparable<? super T>> void recursionSort(T[] array, int startIndex, int endIndex, boolean reverse) {
+        //基础的参数判断与非递归版本一致
+        if (array.length == 0 || array.length == 1) {
+            return;
+        }
+
+        //判断两个索引参数是否符合要求
+        if (startIndex < 0 || startIndex > array.length - 1 || endIndex < 0 || endIndex > array.length - 1 || startIndex > endIndex) {
+            throw new IllegalArgumentException("索引超出范围");
+        }
+
+        //停机条件是startIndex=endIndex
+        if (startIndex != endIndex) {
+            //根据是否降序取最小/最大值对应的索引
+            int swapIndex;
+            if (reverse) {
+                swapIndex = findMaxIndex(array, startIndex, endIndex);
+            } else {
+                swapIndex = findMinIndex(array, startIndex, endIndex);
+            }
+            //交换第一个索引与最大/最小值对应的索引的内容
+            swap(array, startIndex, swapIndex);
+            //对从start+1开始到endIndex的部分继续排序
+            recursionSort(array, startIndex + 1, endIndex, reverse);
+        }
+
+    }
+
+
+    /**
+     * 寻找一个数组在起始索引和结束索引之间的最小值对应的索引, 数组元素需要实现Comparable接口. 私有方法, 为支持排序方法.
+     *
+     * @param array      数组对象
+     * @param startIndex 起始索引
+     * @param lastIndex  结束索引
+     * @param <T>        泛型参数,必须实现Comparable接口
+     * @return 在起始索引和结束索引之间的最小值对应的索引
+     */
+    private static <T extends Comparable<? super T>> int findMinIndex(T[] array, int startIndex, int lastIndex) {
         int min = startIndex;
 
         for (int i = startIndex; i <= lastIndex; i++) {
@@ -34,25 +226,38 @@ public class SelectionSort {
         return min;
     }
 
-    public static <T extends Comparable<? super T>> void resursionSort(T[] array) {
-        recursionFindMinAndSwap(array, 0);
-    }
-        //
-    private static <T extends Comparable<? super T>> void recursionFindMinAndSwap(T[] array, int index) {
-        //停机条件是当前索引等于数组的最大索引
-        if (index != array.length - 1) {
-            //找到当前索引到数组末尾的最小元素对应的索引
-            int minIndex = findMin(array, index, array.length - 1);
 
-            //交换第一个元素和最小元素的值
-            T temp = array[minIndex];
-            array[minIndex] = array[index];
-            array[index] = temp;
-            //然后继续对剩下的数组进行操作
-            recursionFindMinAndSwap(array, index + 1);
+    /**
+     * 寻找一个数组在起始索引和结束索引之间的最大值对应的索引, 数组元素需要实现Comparable接口. 私有方法, 为支持排序方法.
+     *
+     * @param array      数组对象
+     * @param startIndex 起始索引
+     * @param lastIndex  结束索引
+     * @param <T>        泛型参数,必须实现Comparable接口
+     * @return 在起始索引和结束索引之间的最大值对应的索引
+     */
+    private static <T extends Comparable<? super T>> int findMaxIndex(T[] array, int startIndex, int lastIndex) {
+        int max = startIndex;
+
+        for (int i = startIndex; i <= lastIndex; i++) {
+            if (array[max].compareTo(array[i]) < 0) {
+                max = i;
+            }
         }
-
+        return max;
     }
 
-
+    /**
+     * 交换数组中两个元素的方法, 不返回任何值, 就地交换. 私有方法, 为支持排序方法
+     *
+     * @param array  数组
+     * @param index1 要交换的两个元素的其中一个索引
+     * @param index2 要交换的两个元素的另外一个索引
+     * @param <T>    泛型参数,必须实现Comparable接口
+     */
+    private static <T> void swap(T[] array, int index1, int index2) {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
 }
--- src/datastructure/chapter08/Test.java
+++ src/datastructure/chapter08/Test.java
@@ -1,43 +1,92 @@
 package datastructure.chapter08;
 
 import alog4e.chapter02.sort01.Insertion;
-import alog4e.chapter02.sort01.Selection;
+import datastructure.chapter1.ArrayBag;
 import datastructure.ex03.CarA;
 import datastructure.ex03.CarB;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Test {
 
+    public static void fillArray(Integer[] array, int length) {
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt(length * 2);
+        }
+    }
+
     public static void main(String[] args) {
-        CarA carA1 = new CarA(20, "dfkj");
-        CarA carA2 = new CarA(14, "fd");
-        CarA carA3 = new CarA(34, "sad");
-        CarA carA4 = new CarA(20, "asd");
-        CarA carA5 = new CarA(32, "asd");
-        CarA carA6 = new CarA(12, "asd");
 
-        CarB carB1 = new CarB(29, "dfkbbvcj");
-        CarB carB2 = new CarB(21, "bjkd");
-        CarB carB3 = new CarB(33, "bjsakd");
-        CarB carB4 = new CarB(22, "asd");
+        Random random = new Random();
+        int count = 20;
+        Integer[] array = new Integer[count];
 
 
-        CarA[] carAS = new CarA[]{carA1, carA2, carA3, carA4, carA5, carA6};
-        CarA[] carAS2 = new CarA[]{carA1, carA2, carA3, carA4, carA5, carA6};
-        CarA[] carAS3 = new CarA[]{carA1,carA2};
-        CarB[] cars = new CarB[]{carB1, carB2,carB3,carB4};
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sort(array);
+        System.out.println("升序全排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sortDesc(array);
+        System.out.println("降序全排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
 
-        System.out.println(Arrays.toString(carAS));
-        InsertionSort.insertionRecursionSort(carAS);
-        System.out.println(Arrays.toString(carAS));
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        int startIndex = random.nextInt(array.length);
+        int endIndex = startIndex + random.nextInt(array.length / 2 + 1);
+        if (endIndex > array.length - 1) {
+            endIndex = array.length - 1;
+        }
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sortBetweenIndex(array, startIndex, endIndex);
+        System.out.println("在索引 " + startIndex + " 和 " + endIndex + " 之间升序排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sortBetweenIndexDesc(array, startIndex, endIndex);
+        System.out.println("在索引 " + startIndex + " 和 " + endIndex + " 之间降序排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
+        System.out.println();
 
-        System.out.println(Arrays.toString(carAS2));
-        InsertionSort.insertionSort(carAS2);
-        System.out.println(Arrays.toString(carAS2));
-//
-//        System.out.println(Arrays.toString(carAS3));
-//        InsertionSort.insertionRecursionSort(carAS3);
-//        System.out.println(Arrays.toString(carAS3));
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        int n = random.nextInt(array.length - 1 + 1);
+        SelectionSort.sortFromStart(array, n);
+        System.out.println("对数组的前 " + n + " 个元素升序排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sortFromStartDesc(array, n);
+        System.out.println("对数组的前 " + n + " 个元素降序排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sortFromEnd(array, n);
+        System.out.println("对数组的后 " + n + " 个元素升序排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        fillArray(array, array.length);
+        System.out.println("原始数组是: \n" + Arrays.toString(array));
+        SelectionSort.sortFromEndDesc(array, n);
+        System.out.println("对数组的后 " + n + " 个元素降序排列的结果是: \n" + Arrays.toString(array));
+        System.out.println("----------------------------------------------");
     }
 }
