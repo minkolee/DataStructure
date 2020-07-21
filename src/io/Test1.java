83fd2419cb56daa7f6e4d127e008d30ff7e025e3
minkolee <minkolee@gmail.com>
2020-07-21 13:37:01 +0800

    异步部分看完了

--- src/io/Test1.java
+++ src/io/Test1.java
@@ -1,18 +1,37 @@
 package io;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 public class Test1 {
 
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("d:\\downloads");
+        //从目标文件中创面
+        FileInputStream fileInputStream = new FileInputStream("D:\\downloads\\music\\王菲\\2000《寓言》内地引进版\\寓言 内地引进版.cue");
+        FileChannel channel = fileInputStream.getChannel();
+
+
+
+        //旧文件
+        FileChannel newChannel = new FileOutputStream(new File("new.txt")).getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(100);
+
+        while (channel.read(buffer) != -1) {
+            //做好写入准备
+            buffer.flip();
+            newChannel.write(buffer);
+            //写入完成后清除文件
+            buffer.clear();
+        }
+
+        channel.close();
+        newChannel.close();
 
     }
 
